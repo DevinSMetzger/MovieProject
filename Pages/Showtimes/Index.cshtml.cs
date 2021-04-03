8ce0b53a17e61fd9f232b6d7cb4acddf40b16c2b
DevinSMetzger <devinsmetzger@gmail.com>
2021-04-03 15:23:11 -0500

    Updated search function and navigation between pages through filters

--- Pages/Showtimes/Index.cshtml.cs
+++ Pages/Showtimes/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Movie.Data;
 using Movie.Models;
@@ -13,6 +14,14 @@ namespace Movie.Pages.Showtimes
     public class IndexModel : PageModel
     {
         private readonly Movie.Data.MovieContext _context;
+        public IList<Showtime> ShowtimesItem { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string TitleSearch { get; set; }
+
+        public SelectList LocationItems { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string LocationSearch { get; set; }
 
         public IndexModel(Movie.Data.MovieContext context)
         {
@@ -23,7 +32,25 @@ namespace Movie.Pages.Showtimes
 
         public async Task OnGetAsync()
         {
-            Showtime = await _context.Showtime.ToListAsync();
+            IQueryable<string> locationQuery = from m in _context.Showtime
+                                            orderby m.TheaterLocation
+                                            select m.TheaterLocation;
+
+            var showtimes = from m in _context.Showtime
+                         select m;
+
+            if (!string.IsNullOrEmpty(LocationSearch))
+            {
+                showtimes = showtimes.Where(x => x.TheaterLocation == LocationSearch);
+            }
+
+            if (!string.IsNullOrEmpty(TitleSearch))
+            {
+                showtimes = showtimes.Where(s => s.movieTitle.Contains(TitleSearch));
+            }
+            LocationItems = new SelectList(await locationQuery.Distinct().ToListAsync());
+            ShowtimesItem = await showtimes.ToListAsync();
+
         }
     }
 }
